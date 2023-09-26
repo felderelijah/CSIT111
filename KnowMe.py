28617a299e007decd6732bdd2ad5189e7110f898
felderelijah <112753774+felderelijah@users.noreply.github.com>
2023-09-26 02:22:21 -0400

    Add files via upload

---
+++ KnowMe.py
@@ -0,0 +1,22 @@
+# this is fun facts about yourself
+print ('Hello world')
+
+print ('My major is Information Technology.')
+print ('I plan on working in tech.')
+print ('Programming is fun.')
+
+age = 22
+
+print ('I am',age, 'years old') 
+print ()
+name = input('Guess who I am: ')
+print ()
+
+print ("How do you know it's", name)
+
+hours = int(input('How many hours do you work every a week?'))
+print()
+print ('So you work', hours, "hours every week, fair.")
+print()
+payrate = float(input('How much do you get paid? '))
+print ('So you get', payrate, 'an hour.')
